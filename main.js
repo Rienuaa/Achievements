75edeaf86a71851a7e7397ec589e6846c6077532
Q <aaacantor@gmail.com>
2023-07-12 14:52:59 -0400

    first API calls for generic data collection

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 var APIKey = "195F7C54-FC5E-6F48-BE42-68D9169CE12747022F1E-A68E-463D-9F3C-D746C29BC711";
+var APIURL = "https://api.guildwars2.com/v2/";
 
 document.addEventListener('DOMContentLoaded', function() {
     ShowStats();
 }, false);
 
-function ShowStats()
+async function ShowStats()
 {
-  let total = GetTotalAchievedAchievements();
+  let data = await GetAccountAchievementData();
+  
+  let total = data.length;
   
   document.getElementById("total").innerHTML = document.getElementById("total").innerHTML.replace("$VALUE1$", total);
 }
 
-function GetTotalAchievedAchievements()
+async function GetAccountAchievementData()
 {
-  return 10;
+  const response = await fetch(APIURL + "achievements");
+  
+  const data = await response.json();
+  
+  return data;
 }
